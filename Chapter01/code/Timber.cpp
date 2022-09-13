39b59b2e46d810294fe6ff9a5ab56d53d399316b
admin <w1757900@apps.losrios.edu>
2022-09-13 13:24:06 -0700

    yo

--- Chapter01/code/Timber.cpp
+++ Chapter01/code/Timber.cpp
@@ -8,63 +8,55 @@ using namespace std;
 
 int main()
 {
-	// Create a video mode object
+	
 	VideoMode vm(1920, 1080);
 
-	// Create and open a window for the game
 	RenderWindow window(vm, "Timber!!!", Style::Default);
 
-	// Create a texture to hold a graphic on the GPU
-	Texture textureBackground;
 
-	// Load a graphic into the texture
-	textureBackground.loadFromFile("graphics/background.png");
-
-	// Create a sprite
-	Sprite spriteBackground;
-
-	// Attach the texture to the sprite
-	spriteBackground.setTexture(textureBackground);
-
-	// Set the spriteBackground to cover the screen
-	spriteBackground.setPosition(0, 0);
+	
+	
 
 
 	while (window.isOpen())
 	{
 
-		/*
-		****************************************
-		Handle the players input
-		****************************************
-		*/
+
+		 
 
 		if (Keyboard::isKeyPressed(Keyboard::Escape))
 		{
 			window.close();
 		}
+		
+		//ifmouseclicked
+        
 
-		/*
-		****************************************
-		Update the scene
-		****************************************
-		*/
+        sf::RectangleShape dot(sf::Vector2f(200, 40));
+
+        dot.setSize(sf::Vector2f(10, 10));
+        dot.setPosition(800, 500);
+
+		//mousebuttonpress
+
+       
+            sf::Event event;
+
+            while (window.pollEvent(event))
+            {
+                switch (event.type)
+                {
+                case sf::Event::Closed:
+                    window.close();
+                    break;
+                }
+            }
+            window.clear();
+            window.draw(dot);
+            window.display();
 
 
-		/*
-		****************************************
-		Draw the scene
-		****************************************
-		*/
-
-		// Clear everything from the last frame
-		window.clear();
-
-		// Draw our game scene here
-		window.draw(spriteBackground);
-
-		// Show everything we just drew
-		window.display();
+		
 
 
 	}
